23eb75eb1145ce63f6b680b6edda9c9536aa7656
TrejGun <trejgun@gmail.com>
2020-08-18 10:46:02 +0800

    update node types

--- src/env.ts
+++ src/env.ts
@@ -5,7 +5,6 @@ config();
 
 // This namespace redefines one from @types/node to reflect values from .env
 declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace NodeJS {
     // eslint-disable-next-line @typescript-eslint/naming-convention
     interface ProcessEnv {
